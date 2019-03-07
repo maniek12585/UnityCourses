cd647be328bf65a2358918393555a461fa6e90dd
maniek12585 <p.zacharka@gmail.com>
2019-03-07 23:48:58 +0100

    creating repository for new unity courses , this one is 2D unity course

---
+++ Unity_2D_Course/Number_Wizard_UI/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField]int max ;
+    [SerializeField]int min ;
+    [SerializeField] TextMeshProUGUI guessText;  //displaying text on text mesh pro 
+    int guess ;
+    int guessedNumber;
+    void StartGame()
+    {
+        NextGuess();
+    }
+    void NextGuess()
+    {
+        guess=Random.Range(min,max+1);
+        guessText.text=guess.ToString();
+    }
+    public void OnPressHigher()
+    {
+        min = guess+1;
+        NextGuess();
+    }
+    public void OnPressLower()
+    {
+        max = guess-1;
+        NextGuess();
+    }
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        StartGame();
+    }
+
+}
---
+++ Unity_2D_Course/Text101/Text101/Assets/Scripts/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//creating State option in creating menu in Unity
+[CreateAssetMenu(menuName="State")]
+public class State : ScriptableObject
+{   
+    //first num is describing how large is the field that we type in
+    //second num is describing how many lines will fit before its start scrolling
+    [TextArea(14,10)] [SerializeField] string storyText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+     public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
